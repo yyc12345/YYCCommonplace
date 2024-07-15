b912be082c7d71f9f2776483c97f0bc91f648f4f
yyc12345 <yyc12321@outlook.com>
2024-07-15 13:47:14 +0800

    doc: add documentation for COMHelper

--- src/COMHelper.hpp
+++ src/COMHelper.hpp
@@ -9,25 +9,6 @@
 #include <shlobj_core.h>
 #include "WinImportSuffix.hpp"
 
-/**
- * @brief COM fucntions related namespace.
- * @details
- * This namespace is Windows specific and is unavailable on other platforms.
- * 
- * This namespace contain a COM Guard which make sure COM was initialized in current module when loading current module.
- * It is essential because all calling to COM functions should be under the premise that COM has been initialized.
- * This guard also will uninitialize COM when unloading this module.
- * 
- * This namespace also provided various memory-safe types for interacting with COM functions.
- * Although Microsoft also has similar smart pointer called \c CComPtr.
- * But this library is eager to hide all Microsoft-related functions calling.
- * Using \c CComPtr is not corresponding with the philosophy of this library.
- * So these std-based smart pointer type were created.
- * 
- * This namespace is used by internal functions as intended.
- * They should not be used outside of this library.
- * But if you compel to use them, it is also okey.
-*/
 namespace YYCC::COMHelper {
 
 	/**
