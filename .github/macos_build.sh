6449ae197712a74fb4a6c01a1711430eb300075c
yyc12345 <yyc12321@outlook.com>
2026-01-22 16:24:21 +0800

    chore: use github provided package to fetch dependencies

--- .github/macos_build.sh
+++ .github/macos_build.sh
@@ -10,7 +10,7 @@ mkdir install
 cd build
 
 # Build in Release mode
-cmake -DCMAKE_BUILD_TYPE=Release -DYYCC_BUILD_TEST=ON -DGTest_ROOT=$GTest_ROOT ../..
+cmake -DCMAKE_BUILD_TYPE=Release -DYYCC_BUILD_TEST=ON -DGTest_ROOT=$GTest_ROOT -DYYCC_BUILD_BENCHMARK=ON -Dbenchmark_ROOT=$benchmark_ROOT ../..
 cmake --build .
 cmake --install . --prefix=../install
 
--- .github/windows_build.bat
+++ .github/windows_build.bat
@@ -9,7 +9,7 @@ MKDIR install
 CD build
 
 :: Build with x64 architecture in Release mode
-cmake -A x64 -DYYCC_BUILD_TEST=ON -DGTest_ROOT=%GTest_ROOT% ../..
+cmake -A x64 -DYYCC_BUILD_TEST=ON -DGTest_ROOT=%GTest_ROOT% -DYYCC_BUILD_BENCHMARK=ON -Dbenchmark_ROOT=%benchmark_ROOT% ../..
 cmake --build . --config Release
 cmake --install . --prefix=../install --config Release
 
